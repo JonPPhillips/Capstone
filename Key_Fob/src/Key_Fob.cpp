843e83f1c05631348c42ed6e63fc342dd8a1a881
Jon Phillips <Jphillips39@cnm.edu>
2023-11-30 15:05:21 -0700

    weirdness

--- Key_Fob/src/Key_Fob.cpp
+++ Key_Fob/src/Key_Fob.cpp
@@ -7,29 +7,48 @@
 
 #include "Particle.h"
 #include <NeoPixel.h>
-SYSTEM_MODE(SEMI_AUTOMATIC);
+#include <Adafruit_MQTT.h>
+#include <Adafruit_MQTT/Adafruit_MQTT_SPARK.h>
+#include <Adafruit_MQTT/Adafruit_MQTT.h>
+#include <credentials.h>
+#include <math.h>
+SYSTEM_MODE(AUTOMATIC);
+
+void bleConnect();
+void onDataReceived(const uint8_t *data, size_t len, const BlePeerDevice &peer, void *context);
+void MQTT_connect();
+bool MQTT_ping();
 const int PIXELCOUNT = 1;
+
 Adafruit_NeoPixel pixel ( PIXELCOUNT, SPI1 , WS2812B );
-int BUZZER = D16;
+
+
 const BleUuid serviceUuid("6E400001-B5A3-F393-E0A9-E50E24DCCA9E");
 const BleUuid rxUuid("6E400002-B5A3-F393-E0A9-E50E24DCCA9E");
 const BleUuid txUuid("6E400003-B5A3-F393-E0A9-E50E24DCCA9E");
-
 const size_t SCAN_RESULT_MAX = 20;
-BleScanResult scanResults[SCAN_RESULT_MAX];
 
+
+BleScanResult scanResults[SCAN_RESULT_MAX];
 BleCharacteristic peerRxCharacteristic;
 BleCharacteristic peerTxCharacteristic;
 BlePeerDevice peer;
-void bleConnect();
-void onDataReceived(const uint8_t *data, size_t len, const BlePeerDevice &peer, void *context);
 BleCharacteristic rxCharacteristic ("rx",BleCharacteristicProperty::WRITE_WO_RSP,rxUuid,serviceUuid,onDataReceived,NULL);
 uint8_t i;
+
+TCPClient TheClient; 
+Adafruit_MQTT_SPARK mqtt(&TheClient,AIO_SERVER,AIO_SERVERPORT,AIO_USERNAME,AIO_KEY); 
+Adafruit_MQTT_Publish pubFeedLt = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/SafeChildLight");
+Adafruit_MQTT_Publish pubFeedTxt = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/SafeChildText");
+const int BUZZER = D16;
 int count;
 int RSSI;
 int BuckleCheck;
 bool lastBuckleCheck;
 int timer;
+int sinWave;
+int lastTime;
+float t;
 
 
 
@@ -44,6 +63,12 @@ void setup() {
     pixel.show();
     pixel.setBrightness(100);
     pinMode (BUZZER,OUTPUT);
+
+    WiFi.on();
+    WiFi.connect();
+    while(WiFi.connecting()) {
+    Serial.printf(".");
+    }
    
  
 
@@ -52,7 +77,8 @@ void setup() {
 
 
 void loop() {   
-
+    MQTT_connect();
+    MQTT_ping();
     bleConnect();
 
        
@@ -71,8 +97,11 @@ void loop() {
                         pixel.setBrightness(100);
                         pixel.setPixelColor(0,0xff0000);
                         pixel.show();
-                        tone(BUZZER,3300,500);
-                        delay(250);
+                        t = millis()/1000.0;
+                        sinWave = 500*sin(2*M_PI*(2)*t)+3000;
+                        tone(BUZZER,sinWave,100);
+                        delay(90);
+                        noTone(BUZZER);                        
                         pixel.clear();
                         pixel.show();
                         delay(250);
@@ -86,7 +115,15 @@ void loop() {
                     bleConnect();
                }
    
-
+    if((millis()-lastTime) > 5000) {
+        lastTime = millis();
+        Serial.printf("TRIGGERED!!!!!\n");
+        if(mqtt.Update()) {
+            pubFeedLt.publish(lastBuckleCheck);
+            Serial.printf("Publishing ---- %i \n",lastBuckleCheck); 
+        } 
+   
+     }
  
          
 
@@ -121,12 +158,6 @@ void onDataReceived(const uint8_t *data, size_t len, const BlePeerDevice &peer, 
         }
 
         Serial.printf("BuckleCheck ----  %i\n",BuckleCheck);
-        
-  
-        
-
-
-
     }
 }
 
@@ -143,5 +174,39 @@ void bleConnect(){
             }
         }
     }
+}
 
+void MQTT_connect() {
+  int8_t ret;
+ 
+  // Return if already connected.
+  if (mqtt.connected()) {
+    return;
+  }
+ 
+  Serial.print("Connecting to MQTT... ");
+ 
+  while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected
+       Serial.printf("Error Code %s\n",mqtt.connectErrorString(ret));
+       Serial.printf("Retrying MQTT connection in 5 seconds...\n");
+       mqtt.disconnect();
+       delay(5000);  // wait 5 seconds and try again
+  }
+  Serial.printf("MQTT Connected!\n");
+}
+
+bool MQTT_ping() {
+  static unsigned int last;
+  bool pingStatus;
+
+  if ((millis()-last)>120000) {
+      Serial.printf("Pinging MQTT \n");
+      pingStatus = mqtt.ping();
+      if(!pingStatus) {
+        Serial.printf("Disconnecting \n");
+        mqtt.disconnect();
+      }
+      last = millis();
+  }
+  return pingStatus;
 }
